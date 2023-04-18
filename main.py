7f467fc6d22059df835d6889f829e3db8924dcca
Rianone <84690507+Rianone@users.noreply.github.com>
2023-04-18 21:05:12 +0100

    Added icon, title ang background

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ screen = pygame.display.set_mode((800,600))
 
 running = True
 
+#Title and Image game icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("fusee.png")
+pygame.display.set_icon(icon)
+
 #Game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #Screen fill, rgb values
+    screen.fill((0, 0, 0))
+    pygame.display.update()
