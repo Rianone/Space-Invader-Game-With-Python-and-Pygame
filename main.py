d29529b8826a5e75179519868702929f10790612
Rianone <84690507+Rianone@users.noreply.github.com>
2023-04-18 22:43:57 +0100

    Added enemy images, movement and position

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 import pygame
+import random
 
-#initialised pygame
+# initialised pygame
 pygame.init()
 
-#Create the screen width * height
-screen = pygame.display.set_mode((800,600))
+# Create the screen width * height
+screen = pygame.display.set_mode((800, 600))
 
 running = True
 
-#Title and Image game icon
+# Title and Image game icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("fusee.png")
 pygame.display.set_icon(icon)
 
-#Player image info
+# Player image info
 playerImg = pygame.image.load("avatar.png")
 playerX = 370
 playerY = 480
 playerX_change = 0
-playerY_change = 0
 
-#Inserting player
-def player(x,y):
+# Enemy image info
+enemyImg = pygame.image.load("alien" + str(random.randint(1, 4)) + ".png")
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 40
+
+
+# Inserting player
+def player(x, y):
     screen.blit(playerImg, (x, y))
 
-#Game loop
+
+# Inserting Enemyplayer
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+# Game loop
 while running:
-    #Screen fill, rgb values
+    # Screen fill, rgb values
     screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        #Key pressed event condition, check whether is right or left
+        # Key pressed event condition, check whether is right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX_change = -0.3
@@ -45,14 +59,29 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-    #5 = 5 + -0.1 => 5 - 0.1
-    #5 = 5 + 0.1
+    # 5 = 5 + -0.1 => 5 - 0.1
+    # 5 = 5 + 0.1 Player movements
     playerX += playerX_change
 
+    #Checking for player boundaries
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
-    player(playerX,playerY)
+
+    #Enemy movement
+    enemyX += enemyX_change
+    #Checking for enemy boundaries
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
+    # Adding player
+    player(playerX, playerY)
+    # Adding enemy
+    enemy(enemyX, enemyY)
 
     pygame.display.update()
