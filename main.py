bd3866fd5e9f48225726d369def758ef96fb5f8a
Rianone <84690507+Rianone@users.noreply.github.com>
2023-04-18 21:27:28 +0100

    Added player

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 #initialised pygame
 pygame.init()
 
-#Create the screen
+#Create the screen width * height
 screen = pygame.display.set_mode((800,600))
 
 running = True
@@ -13,12 +13,23 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("fusee.png")
 pygame.display.set_icon(icon)
 
+#Player image info
+playerImg = pygame.image.load("avatar.png")
+playerX = 370
+playerY = 480
+
+#Inserting player
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 #Game loop
 while running:
+    #Screen fill, rgb values
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #Screen fill, rgb values
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
